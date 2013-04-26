1d8e3ea2a4fe285f4d35505b495b960a21cf9d11
ollimacgja <ollimacgja@gmail.com>
2013-04-26 00:20:12 -0300

    Final - Not working form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Blog::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
+  resources :comments do 
+    resources :comments
+  end
 
   get "home/index"
 
